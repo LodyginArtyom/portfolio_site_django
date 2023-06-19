df65e8555fb20ebaa44bd3e67417ab3c273de91e
LodyginArtyom <articrafter123@mail.ru>
2023-06-20 00:48:25 +0300

    correct info

--- model/views.py
+++ model/views.py
@@ -1,6 +1,6 @@
 
 from django.views.generic import ListView
-from .models import Projects
+from .models import Projects, Language, Libraries
 
 
 class MovieView(ListView):
@@ -8,6 +8,12 @@ class MovieView(ListView):
     model = Projects
     queryset = Projects.objects.filter(draft=False)
     template_name = "model/index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['languages'] = Language.objects.all()
+        context['libraries'] = Libraries.objects.all()
+        return context
 
 
 
